dbe70b323e31fee7adb11172e295c501e3a99c88
Umur Alpay <umuralpay@outlook.com>
2021-09-11 14:41:30 +0200

    Removed unnecessary console logs

--- src/__tests__/dates.test.ts
+++ src/__tests__/dates.test.ts
@@ -2,12 +2,10 @@ import { getUTCDate } from '../index';
 
 test('Getting UTC date', () => {
   const date = getUTCDate();
-  console.log(date);
   expect(date).toBeDefined();
 });
 
 test('Getting UTC date as epoch', () => {
   const date = getUTCDate(true);
-  console.log(date);
   expect(date).toBeDefined();
 });
